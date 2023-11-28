ab90be469273864a3d848124a5bc1a2d36379e6c
lemwaizz <mwaidennis26@gmail.com>
2023-11-28 13:49:29 +0200

    cont

--- shell.py
+++ shell.py
@@ -1,3 +1,17 @@
 class Menu:
     """Displays the countries to choose from."""
-    pass
+    
+    
+    Countries = [Rwanda, Kenya, Ethiopia, Nigeria, Ghana, Libya, Egypt, South Africa, Zambia, DRC]
+    
+    def __init__(self, selection):
+        self.__selection = selection
+    
+    @property
+    def selection(self):
+        """assigns selction to selection"""
+        return self.__selection
+
+    @selection.setter
+        
+    
