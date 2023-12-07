b2fd29f16f9c782a1706d8f60ef682cbf5e87bdc
l-isaro <l.isaro@alustudent.com>
2023-12-07 13:05:37 +0200

    created a database and table

--- shell.py
+++ shell.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb = mysql.connector.connect(host='DESKTOP-CNV5OOO', user='root', passwd='xxxxxxx')
+mydb = mysql.connector.connect(host='localhost', user='root', passwd='password123')
 
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE books")
